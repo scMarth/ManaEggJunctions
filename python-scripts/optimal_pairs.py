17987ae9ee3a89f85af3a63ba0058b8a67f7215c
scMarth <vlantaca@gmail.com>
2018-08-15 15:53:35 -0700

    rough prototype, may or may not be correct

--- python-scripts/optimal_pairs.py
+++ python-scripts/optimal_pairs.py
@@ -1,3 +1,37 @@
+'''
+
+In this algorithm, we assume that the minimum junction pair to create an egg can only be from
+a pairing in which both eggs have a lower level than that egg, given that level 1 eggs cost 1,
+and cost is defined as how many level 1 eggs it would take to create that egg.
+
+The algorithm works by iterating from the lowest level eggs (1) to the higher level eggs.
+Because we know the cost of the base case (level 1 eggs have cost 1), we can use them to calculate
+the costs of eggs that use some combination of these eggs.
+
+1.) The cost of an level X egg must be at least X
+
+    Proof of 1.) 
+
+Proof of Correctness (Induction):
+
+If an egg X minimum cost junction pair is one that hasn't been explored, then one of those eggs
+has a level greater to or equal to X, because by the time we get to X, we would have explored all eggs
+with a lower level than X.
+
+Assume that an egg X has a minimum cost junction pair A in which one egg in the pair hasn't been explored, and
+has at least one other pair B in which both eggs are explored. Because at least one egg in the optimal pair
+hasn't been explored, it contains an eg which has a level equal to or greater than the level of X.
+
+Thus, by 1.), the cost of the 'minimum cost' junction pair A is at least X + (X-a). a could be anything...
+
+Pair B, has a cost of at most (X-1) + (X-1) = 2X - 2. 
+
+this algorithm may or may not be correct??
+
+
+'''
+
+
 import egg_data
 import optimal_pairs_data
 
@@ -32,33 +66,85 @@ class Junction():
 
 def min_eggs(egg_name):
     global optimal_pairs_vars
-    if (egg_data[egg_name].egg_level == 1):
+    if (egg_data.egg_data[egg_name]['eggLevel'] == 1):
         return 1
     else:
-        if (optimal_pairs_vars.visited_eggs[egg_name] == 1):
+        # if the egg has been visited before, then we know that it's cost has already been computed
+        if (optimal_pairs_vars.visited_eggs[egg_name] == True):
             return optimal_pairs_vars.min_eggs_to_make[egg_name]
 
-def get_optimal_pairs():
+        costs = []
 
+        for i in range(0,len(egg_data.egg_data[egg_name]['junctionPairs'])):
+            pair = egg_data.egg_data[egg_name]['junctionPairs'][i]
+            left_egg = pair[0]
+            right_egg = pair[1]
+
+            # skip this pair if either the left or right egg hasn't been visited yet
+            if (optimal_pairs_vars.visited_eggs[left_egg] == False or \
+                optimal_pairs_vars.visited_eggs[right_egg] == False):
+                    # print("skip")
+                    continue
+
+            # skip this pair if either the left or right egg has an undefined cost
+            if (optimal_pairs_vars.min_eggs_to_make[left_egg] == -1 or \
+                optimal_pairs_vars.min_eggs_to_make[right_egg] == -1):
+                    # print("skip")
+                    continue
+            
+            left_egg_cost = min_eggs(left_egg)
+            right_egg_cost = min_eggs(right_egg)
+            pair_cost = left_egg_cost + right_egg_cost
+            costs.append(pair_cost)
+            # print(pair_cost)
+
+        # the minimum cost is simply the lowest value in the cost array
+        min_cost = min(costs) if costs else -1
+        # print("min_cost: " + (str(min_cost)))
+
+        for i in range(0,len(egg_data.egg_data[egg_name]['junctionPairs'])):
+            pair = egg_data.egg_data[egg_name]['junctionPairs'][i]
+            left_egg = pair[0]
+            right_egg = pair[1]
+
+            # skip this pair if either the left or right egg hasn't been visited yet
+            if (optimal_pairs_vars.visited_eggs[left_egg] == False or \
+                optimal_pairs_vars.visited_eggs[right_egg] == False):
+                    continue
+
+            left_egg_cost = min_eggs(left_egg)
+            right_egg_cost = min_eggs(right_egg)
+
+            # if the cost of this pair is equal to the minumum cost, then it is an optimal pair
+            # so, we add it to the optimal_pairs array
+            if (left_egg_cost + right_egg_cost == min_cost):
+                optimal_pairs_vars.optimal_pairs[egg_name].append(i)
+                # print("optimal pair: " + str(i))
+
+        # set this egg to visited
+        optimal_pairs_vars.visited_eggs[egg_name] = True
+
+        # store the minimum cost of this egg, so that we won't have to recompute it
+        optimal_pairs_vars.min_eggs_to_make[egg_name] = min_cost
+
+        return min_cost
+
+def get_optimal_pairs():
     for lvl in range(1, len(egg_data.egg_levels)):
+    # for lvl in range(1, 3):
         print ("LEVEL " + str(lvl))
         for i in range(0, len(egg_data.egg_levels[lvl])):
             name = egg_data.egg_levels[lvl][i]
-            print(name)
-            min_eggs(name)
-        print("")
-
-
-
-# for item in egg_data.egg_levels:
-#     print(item)
-
-# for item in optimal_pairs_vars.min_eggs_to_make:
-#     print(str(item) + " : " + str(optimal_pairs_vars.min_eggs_to_make[item]))
-
-# my_egg_node = EggNode('Fenrir')
-
-# print(my_egg_node.min_cost)
+            # print(name)
+            print(name + " : cost: " + str(min_eggs(name)))
+            outStr = ""
+            for pair_index in optimal_pairs_vars.optimal_pairs[name]:
+                outStr = "\t["
+                outStr += egg_data.egg_data[name]['junctionPairs'][pair_index][0]
+                outStr += ", "
+                outStr += egg_data.egg_data[name]['junctionPairs'][pair_index][1]
+                outStr += "]\n"
+            print(outStr)
 
 get_optimal_pairs()
 
